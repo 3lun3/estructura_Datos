92e6d2c072bb7dbc9eb3a04056a68faf2d8500da
zeedor <142868373+zeedor@users.noreply.github.com>
2025-07-08 12:19:23 -0400

    Comentarios utils.h

--- Proyecto2/utils.h
+++ Proyecto2/utils.h
@@ -1,19 +1,22 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <string>
-#include <set>
+#include <string>  
+#include <set>     
 
-// Convierte una palabra a minúsculas
+// Esta función toma una palabra y devuelve una nueva palabra con todos los caracteres en minúsculas.
 std::string aMinuscula(const std::string& palabra);
 
-// Limpia una palabra eliminando signos y dejando solo letras
+// Elimina caracteres no alfabéticos (como números y puntuación) y convierte todo a minúsculas.
 std::string limpiarPalabra(const std::string& palabra);
 
-// Carga las stopwords desde un archivo en un set
+
+// Lee un archivo que contiene stopwords y las almacena en un conjunto para que no haya duplicados
+// Este conjunto de stopwords se usará para filtrar las palabras irrelevantes en las consultas.
 void cargarStopwords(const std::string& nombreArchivo, std::set<std::string>& stopwords);
 
-// Verifica si una palabra está en el set de stopwords
+// Devuelve true si la palabra es una stopword, y false si no lo es.
 bool esStopword(const std::string& palabra, const std::set<std::string>& stopwords);
 
 #endif
+
